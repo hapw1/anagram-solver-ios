17b4310633057fe31ddb9f86d4584520956bc9e1
hapw1 <hapw1.developer@gmail.com>
2022-08-21 15:22:24 +0100

    Initial Commit

---
+++ anagram-solver-ios/anagram_solver_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  anagram_solver_iosApp.swift
+//  anagram-solver-ios
+//
+//  Created by Harry Witcomb on 21/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct anagram_solver_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
